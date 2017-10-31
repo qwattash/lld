aabe1b374573718eb0cd6f74fa13b3d69709c3ae
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-31 11:25:24 +0000

    Fix handling of negative offsets in __cap_relocs
    It is used e.g. by mandoc which has a pointer to way before the start of a
    table so that it can be directly indexed by enum constants (since the
    table only contains data for the last few enumerators)

--- ELF/Arch/Cheri.h
+++ ELF/Arch/Cheri.h
@@ -25,11 +25,11 @@ template <llvm::support::endianness E> struct InMemoryCapRelocEntry {
 };
 
 struct SymbolAndOffset {
-  SymbolAndOffset(SymbolBody *S, uint64_t O) : Symbol(S), Offset(O) {}
+  SymbolAndOffset(SymbolBody *S, int64_t O) : Symbol(S), Offset(O) {}
   SymbolAndOffset(const SymbolAndOffset &) = default;
   SymbolAndOffset &operator=(const SymbolAndOffset &) = default;
   SymbolBody *Symbol = nullptr;
-  uint64_t Offset = 0;
+  int64_t Offset = 0;
 
   // for __cap_relocs against local symbols clang emits section+offset instead
   // of the local symbol so that it still works even if the local symbol table
