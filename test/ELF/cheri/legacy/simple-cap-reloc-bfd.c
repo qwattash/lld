b7cf942e95aaeadd0f6e7a78508a0533aa714766
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-11-12 21:12:00 +0000

    Update tests for multicapsize lit changes

--- test/ELF/cheri/legacy/simple-cap-reloc-bfd.c
+++ test/ELF/cheri/legacy/simple-cap-reloc-bfd.c
@@ -1,6 +1,6 @@
 // REQUIRES: clang, cheri_ld_bfd
 
-// RUN: %clang_cheri_purecap_legacy_capreloc %S/simple-cap-reloc.c -c -o %t.o
+// RUN: %cheri_purecap_clang %legacy_caprelocs_flag %S/simple-cap-reloc.c -c -o %t.o
 // RUN: llvm-readobj -s -r %t.o | FileCheck -check-prefix READOBJ %S/simple-cap-reloc-common.check
 // RUN: llvm-objdump -C -r %t.o | FileCheck -check-prefix DUMP-OBJ %S/simple-cap-reloc-common.check
 
@@ -10,7 +10,7 @@
 
 // same again for statically dynamically linked exe:
 // Create a dummy shared library otherwise bfd won't create a dynamic executable
-// RUN: %clang_cheri_purecap_legacy_capreloc %S/../Inputs/dummy_shlib.c -c -o %T/bfd_dummy_shlib.o
+// RUN: %cheri_purecap_clang %legacy_caprelocs_flag %S/../Inputs/dummy_shlib.c -c -o %T/bfd_dummy_shlib.o
 // RUN: %clang_link_purecap_bfd -external-capsizefix %T/bfd_dummy_shlib.o -shared -o %T/libbfd_dummy_shlib.so
 // RUN: %clang_link_purecap_bfd -external-capsizefix -L %T -lbfd_dummy_shlib %t.o -o %t-dynamic.exe
 // RUN: llvm-objdump -h -r -t -C %t-dynamic.exe | FileCheck -check-prefixes DUMP-EXE,DYNAMIC %S/simple-cap-reloc-common.check
